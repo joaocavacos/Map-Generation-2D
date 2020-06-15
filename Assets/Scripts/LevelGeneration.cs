289283a9f74b0238b9ea22736c23ca5422598fa0
João Cavacos <cavacosdoesgaming@gmail.com>
2020-06-15 02:19:50 +0100

    Generate sprites in spawn points

--- Assets/Scripts/LevelGeneration.cs
+++ Assets/Scripts/LevelGeneration.cs
@@ -5,14 +5,13 @@ using UnityEngine;
 public class LevelGeneration : MonoBehaviour
 {
 
-    void Start()
-    {
-        
-    }
+	public GameObject[] objects; //Grab the prefabs and drop it on the objects array on Unity
 
-    void Update()
-    {
-        
-    }
+	void Start()
+	{
+		int r = Random.Range(0, objects.Length); //Picks a random object from the objects array
+		Instantiate(objects[r], transform.position, Quaternion.identity); //Spawn object in random way in the position of the spawn point
+	}
+
 
 }
